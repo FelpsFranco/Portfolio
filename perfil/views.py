af5b9694526437c0e66ea61bcb5aa45f6e5283dc
FelpsFranco <felipefranco.morg@outlook.com>
2023-04-14 08:56:47 -0300

    Portfolio

---
+++ perfil/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def experiencia(request):
+    return render(request, 'experiencia.html')
+
